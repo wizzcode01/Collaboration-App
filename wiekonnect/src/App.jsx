90f7e729d0dab51fa2079c4c76d303c9422fa0ba
wizzycode <dicksonwisdom005@gmail.com>
2025-09-25 14:51:31 +0100

    login fixed

--- wiekonnect/src/App.jsx
+++ wiekonnect/src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from './Pages/Login'
 import Registration from './Pages/Registration'
+
 function App() {
 
   return (
     <>
-     <Registration/>
-     <Login/>
+    <Router>
+      <Routes> 
+         <Route path="/" element={<Login/>}/>
+         {/* <Route path="/Registration" element={<Registration/>}/> */}
+      </Routes>
+    </Router>
     </>
   )
 }
--- wiekonnect/src/Pages/Registration.jsx
+++ wiekonnect/src/Pages/Registration.jsx
@@ -1,6 +1,10 @@
 import { FaArrowRight, FaArrowLeft, FaGoogle, FaApple } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Registration = () => {
-    return (
+
+    if(step === 1) {
+      return (
         <div className="min-h-screen w-full relative flex flex-col items-center justify-center  bg-black">
             <div className="w-full flex gap-5 justify-center items-center ">
                 <h1 className="text-white font-bold text-4xl border border-white rounded-lg p-1 px-4 cursor-pointer"><FaArrowLeft size={30}/></h1> 
@@ -17,7 +21,7 @@ const Registration = () => {
                 <div className="flex flex-col gap-6">
                    <div className="">
                         <input 
-                        type="FirstName"
+                        type="text"
                         name="FirstName"
                         placeholder="FirstName"
                         className="w-full border-b-2 text-white border-gray-200 focus:border-2 focus:border-pink-700 focus:outline-none px-3 py-2"
@@ -25,7 +29,52 @@ const Registration = () => {
                     </div>    
                     <div>
                         <input 
-                        type="LastName"
+                        type="text"
+                        name="LastName"
+                        placeholder="LastName"
+                        className="w-full text-white border-b-2 border-gray-200 focus:border-2 focus:border-pink-700 focus:outline-none px-3 py-2"
+                        />
+                    </div>    
+                </div>
+                <button
+                  className="bg-pink-700 font-bold text-white text-[1.5rem] px-4 py-2 mt-12 m-auto rounded-lg w-56 hover:bg-white hover:text-pink-700 transition-all ease-in-out duration-300 cursor-pointer"
+                  type="submit"
+                >
+                <FaArrowRight size={30} className="text-center m-auto"/>
+                </button>
+            </form>
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-3 p-5">
+           <p
+            className="text-gray-500 text-sm">
+           Already have an account?{" "} <Link to="/" className="text-white">Log In</Link></p> 
+        </div>
+    </div>
+    )
+}
+
+  if( step === 2 ){
+    return (
+         <div className="min-h-screen w-full relative flex flex-col items-center justify-center  bg-black">
+            <div className="w-full flex gap-5 justify-center items-center ">
+                <h1 className="text-3xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">Wedic-Connect</h1>
+            </div>    
+           <div className="flex flex-col items-center w-[95%] max-w-md bg-black px-8 py-10 ">
+            <form 
+            className="flex flex-col w-full mt-10 gap-2">
+                <div className="flex flex-col gap-6">
+                   <div className="">
+                        <input 
+                        type="text"
+                        name="FirstName"
+                        placeholder="FirstName"
+                        className="w-full border-b-2 text-white border-gray-200 focus:border-2 focus:border-pink-700 focus:outline-none px-3 py-2"
+                        />
+                    </div>    
+                    <div>
+                        <input 
+                        type="text"
                         name="LastName"
                         placeholder="LastName"
                         className="w-full text-white border-b-2 border-gray-200 focus:border-2 focus:border-pink-700 focus:outline-none px-3 py-2"
@@ -44,12 +93,12 @@ const Registration = () => {
         </div>
         <div className="flex flex-col justify-center items-center gap-3 mt-3 p-5">
            <p
-            to="/Registration"
             className="text-gray-500 text-sm">
-           Already have an account? <a className="text-white">Log In</a></p> 
+           Already have an account?{" "} <Link to="/" className="text-white">Log In</Link></p> 
         </div>
     </div>
     )
+  }
 }
 
 export default Registration
